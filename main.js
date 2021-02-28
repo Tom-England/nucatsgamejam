3e3a7937d00a56a3e6a9f1512bacb4d367fa58be
Tom-England <tomengland6679@gmail.com>
2021-02-28 22:24:43 +0000

    Initial Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("main.js loaded");
+
+function startGame() {
+  myGameArea.start();
+}
+
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 1280;
+    this.canvas.height = 720;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+}
